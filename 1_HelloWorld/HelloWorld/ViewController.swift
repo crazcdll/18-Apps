d8c3b1954837b2d56b97c2f3e7d655682726b1ec
zcdll <qinsword521@gmail.com>
2016-01-22 23:53:08 +0800

    modify creator

--- 1_HelloWorld/HelloWorld/ViewController.swift
+++ 1_HelloWorld/HelloWorld/ViewController.swift
@@ -2,18 +2,18 @@
 //  ViewController.swift
 //  HelloWorld
 //
-//  Created by ZC on 16/1/5.
-//  Copyright © 2016年 ZC. All rights reserved.
+//  Created by zcdll on 16/1/5.
+//  Copyright © 2016年 zcdll. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var textField: UITextField!
-    
+
     @IBOutlet weak var label: UILabel!
-    
+
     @IBAction func tap(sender: AnyObject) {
         print("tapped")
         label.text = textField.text
